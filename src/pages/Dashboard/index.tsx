a5691a8b3c2da706e1df630623027c4c35eb80e2
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2021-12-29 19:03:54 -0300

    Header infos

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import { Container, Title } from './styles'
+import React from 'react'
+import { StatusBar } from 'react-native'
+import theme from '../../global/styles/theme'
+import { Container, Header, HeaderContent, Profile, Avatar, User, Greatings, UserName, LogoutButton } from './styles'
 
 export default function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <StatusBar backgroundColor={theme.colors.primary} />
+            <Header>
+                <HeaderContent>
+                    <Profile>
+                        <Avatar source={{ uri: 'https://avatars.githubusercontent.com/u/72226013?v=4' }} />
+                        <User>
+                            <Greatings>Olá,</Greatings>
+                            <UserName>Rafael</UserName>
+                        </User>
+                    </Profile>
+                    <LogoutButton name="power" />
+                </HeaderContent>
+            </Header>
         </Container>
     )
 }
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,19 +1,62 @@
 import styled from 'styled-components/native'
+import { RFValue, RFPercentage } from 'react-native-responsive-fontsize'
+import { Feather } from '@expo/vector-icons'
 
 const Container = styled.View`
     flex: 1;
-    justify-content: center;
-    align-items: center;
     background-color: ${({theme}) => theme.colors.background};
-`;
+`
 
-const Title = styled.Text`
+const Header = styled.View`
+    width: 100%;
+    height: ${RFPercentage(42)}px;
+    background-color: ${({theme}) => theme.colors.primary};
+
+    flex-direction: row;
+    justify-content: center;
+`
+
+const HeaderContent = styled.View`
+    width: 100%;
+    padding: 0 28px;
+    margin: 0 auto;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`   
+
+const Profile = styled.View`
+    flex-direction: row;
+    align-items: center;
+`
+
+const Avatar = styled.Image`
+    width: ${RFValue(50)}px;
+    height: ${RFValue(50)}px;
+    border-radius: 10px;
+`
+
+const User = styled.View`
+    margin-left: 16px;
+`
+
+const Greatings = styled.Text`
+    color: ${({theme}) => theme.colors.shape};
+    font-size: ${RFValue(14)}px};
+    font-family: ${({theme}) => theme.fonts.regular};
+    line-height: ${RFValue(18)}px};
+`
+
+const UserName = styled.Text`
+    color: ${({theme}) => theme.colors.shape};
+    font-size: ${RFValue(14)}px};
     font-family: ${({theme}) => theme.fonts.bold};
-    color: ${({theme}) => theme.colors.attention};
-    font-size: 32px;
-    padding-bottom: 4px;
-    border-bottom-width: 1px;
-    border-bottom-color: ${({theme}) => theme.colors.attention};
-`;
+    line-height: ${RFValue(18)}px};
+`
 
-export { Container, Title }
+const LogoutButton = styled(Feather)`
+    color: ${({theme}) => theme.colors.secondary};
+    font-size: ${RFValue(28)}px};
+`
+
+export { Container, Header, HeaderContent, Profile, Avatar, User, Greatings, UserName, LogoutButton }
