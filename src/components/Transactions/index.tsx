aa2e20ce14abe3742711fd628d470fb57f53d7c6
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-02-12 09:55:46 -0300

    Fix quando listagem está vazia e useFocusEffect para atualizar

--- src/components/Transactions/index.tsx
+++ src/components/Transactions/index.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { TransactionCard, TransactionCardProps  } from '../TransactionCard'
 import { TransactionsList, Title, TransactionsArea } from './styles'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { categories } from '../../utils/categories'
+import { useFocusEffect } from '@react-navigation/native'
 
 type TransactionFromStorage = {
     id: string
@@ -26,39 +27,44 @@ export function Transactions() {
         const response = await AsyncStorage.getItem(collectionKey)
         const data = response ? JSON.parse(response) : []
 
-        const transactionsFormatted: DataListProps[] = data.transactions.map((transaction: TransactionFromStorage) => {
-            
-            return {
-                id: transaction.id,
-                data: {
-                    name: transaction.name,
-                    amount: Number(transaction.amount).toLocaleString('pt-BR', {
-                        style: 'currency', currency: 'BRL'
-                    }).replace('R$', 'R$ '),
-                    type: transaction.type,
-                    category: categories.find(category => {
-                        if(category.key === transaction.category) {
-                            return {
-                                name: category.name,
-                                icon: category.icon
+        if(data.lenght > 0) {
+            const transactionsFormatted: DataListProps[] = data.transactions.map((transaction: TransactionFromStorage) => {
+                return {
+                    id: transaction.id,
+                    data: {
+                        name: transaction.name,
+                        amount: Number(transaction.amount).toLocaleString('pt-BR', {
+                            style: 'currency', currency: 'BRL'
+                        }).replace('R$', 'R$ '),
+                        type: transaction.type,
+                        category: categories.find(category => {
+                            if(category.key === transaction.category) {
+                                return {
+                                    name: category.name,
+                                    icon: category.icon
+                                }
                             }
-                        }
-                    }),
-                    date: Intl.DateTimeFormat('pt-BR', { 
-                        day: '2-digit', 
-                        month: '2-digit', 
-                        year: '2-digit' 
-                    }).format(new Date(transaction.date))
+                        }),
+                        date: Intl.DateTimeFormat('pt-BR', { 
+                            day: '2-digit', 
+                            month: '2-digit', 
+                            year: '2-digit' 
+                        }).format(new Date(transaction.date))
+                    }
                 }
-            }
-        })
-
-        setTransactions(transactionsFormatted)
+            })
+    
+            setTransactions(transactionsFormatted)
+        }
     }
 
     useEffect(() => {
         loadTransactions()
     }, [])
+
+    useFocusEffect(useCallback(() => {
+        loadTransactions()
+    }, []))
 
     return (
         <TransactionsArea>
