c2e2d3824d2a74762142f636dcbeff14a460c5ff
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-02-13 14:30:50 -0300

    Criando gráfico

--- src/pages/Resume/index.tsx
+++ src/pages/Resume/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { StatusBar } from "react-native";
 import { useFocusEffect } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage"
+import { VictoryPie } from "victory-native"
 import { ResumeContainer } from "./styles";
 import theme from "../../global/styles/theme";
 import { Wrapper } from '../../components/Wrapper'
@@ -9,12 +10,14 @@ import { Header } from "../../components/Header";
 import { HistoryCard } from "../../components/HistoryCard";
 import { categories } from "../../utils/categories";
 import { TransactionFromStorage } from "../../types";
+import { RFValue } from "react-native-responsive-fontsize";
 
 type CategoryData = {
     id: string
     name: string
     color: string
     total: number
+    percentage: string
 }
 
 export function Resume() {
@@ -31,6 +34,8 @@ export function Resume() {
 
             const expensives = transactions.filter(transaction => transaction.type === 'withdraw')
 
+            const expensivesTotal = expensives.reduce((total, expensive) => total + expensive.amount, 0)
+
             const totalByCategory: CategoryData[] = []
 
             categories.forEach(category => {
@@ -43,11 +48,14 @@ export function Resume() {
                 })
 
                 if(categorySum > 0) {
+                    const percentage = (categorySum / expensivesTotal * 100).toFixed(0) + '%'
+
                     totalByCategory.push({
                         id: category.key,
                         name: category.name,
                         color: category.color,
-                        total: categorySum
+                        total: categorySum,
+                        percentage
                     })
                 }
             })
@@ -63,9 +71,18 @@ export function Resume() {
     return (
         <Wrapper>
             <StatusBar backgroundColor={theme.colors.primary} />
-            <Header title="Resumo" />
+            <Header title="Resumo" />            
 
             <ResumeContainer>
+                <VictoryPie 
+                    data={totalByCategories} 
+                    x="percentage" 
+                    y="total" 
+                    colorScale={totalByCategories.map(category => category.color)} 
+                    style={{ labels: { fontSize: RFValue(18), fontWeight: 'bold', fill: '#FFF' } }} 
+                    labelRadius={75} 
+                />
+
                 {totalByCategories.map((category) => <HistoryCard key={category.id} color={category.color} title={category.name} amount={category.total} />)}
             </ResumeContainer>
 
